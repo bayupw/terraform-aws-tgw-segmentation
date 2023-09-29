d57edbcc42793a26fff0cc852695be6c7c50de6a
Bayu Wibowo <bayupw@gmail.com>
2023-09-29 15:42:53 +1300

    Update env-prod-rtb.tf

--- env-prod-rtb.tf
+++ env-prod-rtb.tf
@@ -13,14 +13,16 @@ resource "aws_ec2_transit_gateway_route" "tgw_blackhole_static_routes" {
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.env_prod.id
 }
 
-# Azure routes
+# Azure routes via peering TGW
 resource "aws_ec2_transit_gateway_route" "azure_static_routes" { 
   destination_cidr_block = var.azure_cidr_block
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.env_prod.id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.peering.id
 }
 
-# onprem routes
+# onprem routes via peering TGW
 resource "aws_ec2_transit_gateway_route" "onprem_static_routes" { 
   destination_cidr_block = var.onprem_cidr_block
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.env_prod.id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.peering.id
 }
